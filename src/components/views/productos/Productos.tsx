050ceee5827109f17379d9461f9a276cae17bc2b
Diego Quevedo <Diego_Quevedo@epam.com>
2022-02-19 04:28:03 -0500

    Ajusta estilos de vista productos

--- src/components/views/productos/Productos.tsx
+++ src/components/views/productos/Productos.tsx
@@ -4,12 +4,20 @@ import CajaBusqueda from '../../cajabusqueda/CajaBusqueda';
 import axios from 'axios';
 import { IProductos, IResponseQuery } from '../../../modelo/interfaces';
 import MigaDePan from '../../migadepan/MigaDePan';
-import { Box } from '@mui/material';
+import { Box, ButtonBase, IconButton, InputBase, Paper, Grid, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import './Productos.scss'
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
 
 const Productos = () => {
     const [searchParams] = useSearchParams();
     const query = searchParams.get('search');
-    console.log('query es: ' + query)
     const [results, setResults] = useState<IProductos>();
     const navigate = useNavigate();
 
@@ -71,17 +79,67 @@ const Productos = () => {
             <CajaBusqueda defaultQuery={query || ''}/>
             {results !== undefined && results.items && results.items?.length > 0 && results.items?.length > 0 &&
                 <>
-                    {results?.categories !== undefined && results?.categories?.length > 0 &&
-                        <Box sx={{ml: 3, mt: 2}}><MigaDePan categorias={results?.categories}/></Box>}
-                    {results?.items.slice(0, 4).map(it =>
-                        <>
-                            <a onClick={() => handleDetail(it.id)}>
-                                <div><p>{it.id}</p></div>
-                            </a>
-                        </>
-                    )}
+                        {results?.categories !== undefined && results?.categories?.length > 0 &&
+                            <Box sx={{ml: 3, mt: 2}}><MigaDePan categorias={results?.categories}/></Box>}
+                        {results?.items.slice(0, 4).map(it =>
+                            <>
+                                    <Paper
+                                        sx={{
+                                            p: 2,
+                                            mt: 2,
+                                            ml: 'auto',
+                                            mr: 'auto',
+                                            mb: 'auto',
+                                            minWidth: 400,
+                                            maxWidth: 600,
+                                            alignItems: 'center',
+                                            backgroundColor: (theme) =>
+                                                theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                                        }}
+                                        key={`${it.id}-paper`}
+                                    ><a className={'href_grid'} onClick={() => handleDetail(it.id)} key={`${it.id}-a`}>
+                                        <Grid container spacing={2} key={`${it.id}-grid-img-container`}>
+                                            <Grid item sx={{width: 128, height: 128}} key={`${it.id}-grid-img`}>
+                                                <Img alt="complex" src={it.picture} key={`${it.id}-img`}/>
+                                            </Grid>
+                                            <Grid item xs={12} sm container sx={{textAlign: 'left'}}
+                                                  key={`${it.id}-grid-desc-container`}>
+                                                <Grid item xs container direction="column" spacing={2}
+                                                      key={`${it.id}-grid-desc`}>
+                                                    <Grid item xs key={`${it.id}-grid-desc-xs`}>
+                                                        <Typography gutterBottom variant="subtitle1" component="div"
+                                                                    key={`${it.id}-grid-typo-sub`}>
+                                                            {Intl.NumberFormat('es-AR', {
+                                                                style: 'currency',
+                                                                currency: 'ARS'
+                                                            }).format(it.price.amount)}
+                                                        </Typography>
+                                                        <Typography variant="body2" gutterBottom
+                                                                    key={`${it.id}-grid-typo-title`}>
+                                                            {it.title}
+                                                        </Typography>
+                                                        {it.free_shipping &&
+                                                            <Typography variant="body2" color="text.secondary"
+                                                                        key={`${it.id}-grid-typo-shipp`}>
+                                                                Envío gratis
+                                                            </Typography>}
+                                                    </Grid>
+                                                </Grid>
+                                                <Grid item key={`${it.id}-grid-condition`}>
+                                                    <Typography variant="subtitle1" component="div"
+                                                                key={`${it.id}-grid-typo-condition`}>
+                                                        {it.condition}
+                                                    </Typography>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                    </a>
+                                    </Paper>
+                            </>
+                        )}
                 </>}
         </Box>
+
 
     );
 }
